46c31422cff5ed837eeb30925243ab3a547d484b
DataGen64 <bkri0003@student.monash.edu>
2019-10-08 19:07:01 +1100

    First Commit

---
+++ SQLQuery_Create table.sql
@@ -0,0 +1,16 @@
+﻿create table [dbo].[Students](
+[Id] int Identity(1,1) NOT NULL,
+[FirstName] nvarchar(max) NOT NULL,
+[LastName] nvarchar(max) NOT NULL
+primary key (Id));
+GO
+
+create table [dbo].[Units](
+[Id] int Identity(1,1) NOT NULL,
+[Name] nvarchar(max) NOT NULL,
+[Description] nvarchar(max) NOT NULL,
+[StudentId] int NOT NULL
+primary key (Id),
+foreign key (StudentID) references Students(Id)
+);
+GO
---
+++ week07/Models/Model1.cs
@@ -0,0 +1,26 @@
+namespace week07.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=FIT5032_Model")
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<Unit> Units { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>()
+                .HasMany(e => e.Units)
+                .WithRequired(e => e.Student)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
